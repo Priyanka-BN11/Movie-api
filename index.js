6dfbc78f03ba55285faffd33d63350fdbb3d6ccc
Priyanka-BN11 <priyankabnpinky@gmail.com>
2022-08-22 17:06:55 +0530

    Heroku update

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ const Genres = Models.Genre;
 const Directors = Models.Director;
 
 //Database connection
-mongoose.connect('mongodb://localhost:27017/test', { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-});
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 //importing cors
