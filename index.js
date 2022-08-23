29adc57d665e62da9f4d67194e5c9b3fe3bb47f0
Priyanka-BN11 <priyankabnpinky@gmail.com>
2022-08-23 17:50:35 +0530

    adding .env file

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
-
+//import dotenv to support .env file contains environment variable
+require('dotenv').config();
 //Database connection
 // mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -31,7 +32,7 @@ let auth = require('./auth')(app);
 //after importing auth, importing passport
 const passport = require('passport');
 require('./passport');
-
+//logging with morgan (middleware)
 app.use(morgan('common'));
 //displays users
 app.get('/users', passport.authenticate('jwt', {session: false}), (req,res) => {
