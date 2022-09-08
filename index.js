89e3e28fb2584f3be49b67a048c510b03f29b653
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2022-09-08 16:47:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -253,13 +253,13 @@ app.use(morgan('common'));
     console.log('Your app is listening on port 8080.');
   });
   
-  let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+  const allowedOrigins = ["http://localhost:1234", "http://localhost:8080"];
 
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      const message = 'The CORS policy for this application does not allow access from origin ' + origin;
       return callback(new Error(message ), false);
     }
     return callback(null, true);
