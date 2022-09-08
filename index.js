43fd703b7b9eecee59acdac75fde092f814dc234
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2022-09-08 16:55:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -253,18 +253,23 @@ app.use(morgan('common'));
     console.log('Your app is listening on port 8080.');
   });
   
-  const allowedOrigins = ["http://localhost:1234", "http://localhost:8080"];
+  // const allowedOrigins = ["http://localhost:1234", "http://localhost:8080"];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      const message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       const message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+const allowedOrigins = ['*'];
+const options= cors.CorsOptions={
+  origin: allowedOrigins
+};
+app.use(cors(options));
  //heroku
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
